47243a10b92bf153dd9bede5277cc4f244f85559
meshnesh <tonnie.nesh@gmail.com>
2017-11-23 10:36:00 +0300

    ignore files in the gitignore file

--- data/events.py
+++ data/events.py
@@ -6,7 +6,19 @@ events = [
         'time':u'11:00AM',
         'date':u'25 NOV 2017',
         'description': u'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,', 
-        'done': False
+        'done': False,
+        'rsvp': [
+            {
+            'user_id': 1,
+            'name': u'John Doe',
+            'email': u'john.D@gmail.com'
+            },
+            {
+            'user_id': 3,
+            'name': u'Antony Ng\'ang\'a',
+            'email': u'tonny.nesh@gmail.com'
+            }
+        ]
     },
     {
         'id': 2,
@@ -15,6 +27,13 @@ events = [
         'time':u'07:00PM',
         'date':u'30 NOV 2017',
         'description': u'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,', 
-        'done': False
+        'done': False,
+        'rsvp': [
+            {
+            'user_id': 2,
+            'name': u'Mary Jane',
+            'email': u'jane.mary@yahoo.com'
+            }
+        ]
     }
 ]
